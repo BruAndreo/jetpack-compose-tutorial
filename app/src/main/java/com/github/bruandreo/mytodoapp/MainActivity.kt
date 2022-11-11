3fa5dbbcc7f27d2c61d532409c7a9c1338e5b13a
Bruno Andreo <brunolino2026@gmail.com>
2022-11-10 22:20:55 -0300

    feat: finished basic tutorial with jetpack compose

--- app/src/main/java/com/github/bruandreo/mytodoapp/MainActivity.kt
+++ app/src/main/java/com/github/bruandreo/mytodoapp/MainActivity.kt
@@ -1,24 +1,45 @@
 package com.github.bruandreo.mytodoapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.github.bruandreo.mytodoapp.ui.theme.MyTodoAppTheme
+import com.github.bruandreo.mytodoapp.ui.theme.shapeScheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            MessageCard("World!")
+            MyTodoAppTheme {
+                Surface {
+                    MessageCard(
+                        msg = Message("Bruno", "Jetpack Compose")
+                    )
+                }
+            }
         }
     }
 }
@@ -34,22 +55,85 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+        )
         
-        Column {
-            Text(text = msg.author)
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapeScheme.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    name = "Light Mode"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Bruno", "Aprendendo Jetpack Compose")
-    )
+    MyTodoAppTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Bruno", "Aprendendo Jetpack Compose")
+            )
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {
+                message -> MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation () {
+    MyTodoAppTheme {
+        Conversation(
+            listOf(
+                Message("Bruno", "Mexendo com listas"),
+                Message("Bruno", "Mexendo com listas, com uma mensagem mais longa para aprender a mexer com animacões"),
+                Message("Bruno", "Mexendo com listas"),
+                Message("Bruno", "Mexendo com listas")
+            )
+        )
+    }
 }
